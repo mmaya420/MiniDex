fca735f178bc27a3ab982424e4815787fb21798b
Kevin <soldierkevin0202@gmail.com>
2024-05-10 08:06:56 -0400

    Initial Commit

---
+++ ProjectPoke/ProjectPokeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectPokeApp.swift
+//  ProjectPoke
+//
+//  Created by Kevin Zapata on 5/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ProjectPokeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
